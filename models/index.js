5b93804acac2e0d54a35f66971113ac543022e20
ZQWhiting <zach.whiting@icloud.com>
2020-07-19 17:38:04 -0600

    created Comment model; created Comment routes; updated Get queries for Users and Posts to show Comment data

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -11,6 +12,9 @@ User.belongsToMany(Post, {
     foreignKey: 'user_id'
 })
 User.hasMany(Vote, {
+    foreignKey: 'user_id'
+})
+User.hasMany(Comment, {
     foreignKey: 'user_id'
 })
 
@@ -25,6 +29,9 @@ Post.belongsToMany(User, {
 Post.hasMany(Vote, {
     foreignKey: 'post_id'
 })
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
 
 Vote.belongsTo(User, {
     foreignKey: 'user_id'
@@ -33,8 +40,16 @@ Vote.belongsTo(Post, {
     foreignKey: 'post_id'
 })
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
 module.exports = {
     User,
     Post,
-    Vote
+    Vote,
+    Comment
 }
